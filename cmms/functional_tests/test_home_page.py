f9d72741522098b735e5b9219b402b71ca940a59
Rafal Piotrowski <rafal.piotrowski@o2.pl>
2019-08-12 22:59:24 +0200

    Test for machines list in home page passed

--- cmms/functional_tests/test_home_page.py
+++ cmms/functional_tests/test_home_page.py
@@ -10,9 +10,6 @@ class FunctionalTest(LiveServerTestCase):
     def setUpClass(self):
         super().setUpClass()
         self.browser = webdriver.Chrome()
-        self.browser.implicitly_wait(10)
-        print(self.live_server_url, file=sys.stderr)
-
 
     @classmethod
     def tearDownClass(self):
@@ -20,9 +17,17 @@ class FunctionalTest(LiveServerTestCase):
         super().tearDownClass()
 
     #User enter home site and see cmms in title
-    def test_Django_in_title(self):
+    def test_cmms_in_title(self):
         self.browser.implicitly_wait(10)
         self.browser.get(self.live_server_url)
         print('łącze do: ', self.live_server_url, file=sys.stderr)
-        self.browser.implicitly_wait(100)
-        self.assertIn('cmms', self.browser.title)
+        self.assertIn('Cmms', self.browser.title)
+
+    #In home page breakdown can be registered
+    #Machine can be chosen from dropdown menu
+    def test_machine_dropdown_list_on_home_page(self):
+        self.browser.get(self.live_server_url)
+        machines_list = self.browser.find_element_by_tag_name("select")
+        all_options = machines_list.find_elements_by_tag_name("option")
+        all_options = [option.get_attribute("text") for option in all_options]
+        self.assertIn('Machine 1', all_options)
