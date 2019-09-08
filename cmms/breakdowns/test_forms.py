9218604c42061010f400e3b6f58822b6740b2571
Rafal Piotrowski <rafal.piotrowski@o2.pl>
2019-09-08 22:42:14 +0200

    functional test and form test rebuild to use set up class

--- cmms/breakdowns/test_forms.py
+++ cmms/breakdowns/test_forms.py
@@ -1,19 +1,23 @@
 from django.test import TestCase
-from django.forms import ValidationError
 
 from .forms import BreakdownForm
-from .models import Machine
+from .models import Machine, Breakdown
 
 import sys
 
 
 class BreakdownFormTest(TestCase):
+    @classmethod
+    def setUpClass(self):
+        super().setUpClass()
+        self.machine = Machine.objects.create(name='Machine 1')
+#        self.start_time = '2009-10-25 14:30'
+
     def test_breakdown_form_renders_dropdown_list(self):
         form = BreakdownForm()
         self.assertIn('select', form.as_p())
 
     def test_breakdown_form_lists_all_machines_to_choose_from(self):
-        Machine.objects.create(name='Machine 1')
         all_machines = [machine.name for machine in Machine.objects.all()]
         form = BreakdownForm()
 
@@ -22,47 +26,44 @@ class BreakdownFormTest(TestCase):
             )
         self.assertEqual(all_machines, list(choice_names)[1:])
 
-    def test_form_validation_for_correct_start_and_end_time(self):
-        machine = Machine.objects.create(name='Machine 1')
+    def test_form_saved_if_data_correct(self):
         start_time = '2009-10-25 14:30'
         end_time = '2009-10-25 15:30'
-        data = {'machine': machine.id, 'start_time': start_time,
+        data = {'machine': self.machine.id, 'start_time': start_time,
                 'end_time': end_time}
         form = BreakdownForm(data)
-        self.assertTrue(form.is_valid())
+        if form.is_valid():
+            new_breakdown = form.save()
+        self.assertEqual(new_breakdown, Breakdown.objects.all()[0])
 
     def test_form_does_not_validate_incorrect_start_time(self):
-        machine = Machine.objects.create(name='Machine 1')
         start_time = '2009-10-25 14:90'
         end_time = '2009-10-25 15:30'
-        data = {'machine': machine.id, 'start_time': start_time,
+        data = {'machine': self.machine.id, 'start_time': start_time,
                 'end_time': end_time}
         form = BreakdownForm(data)
         self.assertFalse(form.is_valid())
 
     def test_form_does_not_accept_start_time_in_future(self):
-        machine = Machine.objects.create(name='Machine 1')
         start_time = '3009-10-25 14:30'
         end_time = '2009-10-25 15:30'
-        data = {'machine': machine.id, 'start_time': start_time,
+        data = {'machine': self.machine.id, 'start_time': start_time,
                 'end_time': end_time}
         form = BreakdownForm(data)
         self.assertFalse(form.is_valid())
 
     def test_form_does_not_accept_end_time_in_future(self):
-        machine = Machine.objects.create(name='Machine 1')
         start_time = '2009-10-25 14:30'
         end_time = '3009-10-25 15:30'
-        data = {'machine': machine.id, 'start_time': start_time,
+        data = {'machine': self.machine.id, 'start_time': start_time,
                 'end_time': end_time}
         form = BreakdownForm(data)
         self.assertFalse(form.is_valid())
 
     def test_for_start_time_before_end_time(self):
-        machine = Machine.objects.create(name='Machine 1')
         start_time = '2009-10-25 14:30'
         end_time = '2009-10-24 15:30'
-        data = {'machine': machine.id, 'start_time': start_time,
+        data = {'machine': self.machine.id, 'start_time': start_time,
                 'end_time': end_time}
         form = BreakdownForm(data)
         self.assertFalse(form.is_valid())
--- cmms/functional_tests/test_home_page.py
+++ cmms/functional_tests/test_home_page.py
@@ -14,41 +14,40 @@ class FunctionalTest(LiveServerTestCase):
     def setUpClass(self):
         super().setUpClass()
         self.browser = webdriver.Chrome()
+        Machine.objects.create(name="Machine 1")
 
     @classmethod
     def tearDownClass(self):
         self.browser.quit()
         super().tearDownClass()
 
-    # User enter home site and see cmms in title
-    def test_cmms_in_title(self):
+
+class BreakdownRegistrationTest(FunctionalTest):
+
+    def test_breakdown_can_be_saved(self):
+        # User enter home site and see cmms in title
         self.browser.implicitly_wait(10)
         self.browser.get(self.live_server_url)
-        print('łącze do: ', self.live_server_url, file=sys.stderr)
         self.assertIn('Cmms', self.browser.title)
 
-    # In home page breakdown can be registered...
-    def test_register_breakdown_on_home_page(self):
-        self.browser.get(self.live_server_url)
+        # In home page user is encouraged to register breakdown...
         self.assertIn('Register breakdown', self.browser.page_source)
 
-    # ...Machine can be chosen from dropdown menu...
-    def test_machine_dropdown_list_on_home_page(self):
-        Machine.objects.create(name="Machine 1")
-        self.browser.get(self.live_server_url)
-        self.browser.implicitly_wait(10)
+        # ...Machine can be chosen from dropdown menu...
         machines_list = self.browser.find_element_by_tag_name("select")
         all_options = machines_list.find_elements_by_tag_name("option")
         all_options = [option.get_attribute("text") for option in all_options]
         self.assertIn('Machine 1', all_options)
 
     # ...and breakdown time can be picked. User enter start and end time...
-    def test_datetime_picker_for_breakdown_time_available(self):
-        self.browser.get(self.live_server_url)
         breakdown_start = self.browser.find_element_by_id('id_start_time')
         breakdown_start.send_keys('2009-10-25 14:30')
         breakdown_end = self.browser.find_element_by_id('id_end_time')
         breakdown_end.send_keys('2009-10-26 14:30')
+
+    # and enter short description of breakdown
+        description = self.browser.find_element_by_id('id_breakdown_description')
+        descriptions.send_keys('Main motor failure')
 
     # and press 'save' button
         self.browser.find_element_by_id('save_breakdown').click()
