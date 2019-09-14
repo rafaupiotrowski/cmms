b650edff7a07702e18a01c84465ab72d8aff061d
Rafal Piotrowski <rafal.piotrowski@o2.pl>
2019-09-14 23:04:26 +0200

    functional test for success message after breakdown saving

--- cmms/breakdowns/views.py
+++ cmms/breakdowns/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 from bootstrap_datepicker_plus import DateTimePickerInput
 
@@ -14,6 +15,7 @@ def home_page(request):
         breakdown_form = BreakdownForm(data=request.POST)
         if breakdown_form.is_valid():
             breakdown_form.save()
+            messages.success(request, 'Breakdown successfully saved.')
     return render(request, 'breakdowns/home.html', {
         "form": breakdown_form,
     })
--- cmms/functional_tests/test_home_page.py
+++ cmms/functional_tests/test_home_page.py
@@ -3,7 +3,7 @@ import sys
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import Select
 
 from breakdowns.models import Machine
 
@@ -39,6 +39,10 @@ class BreakdownRegistrationTest(FunctionalTest):
         all_options = [option.get_attribute("text") for option in all_options]
         self.assertIn('Machine 1', all_options)
 
+        # user choose Machine 1 option
+        select = Select(self.browser.find_element_by_id('id_machine'))
+        select.select_by_visible_text('Machine 1')
+
     # ...and breakdown time can be picked. User enter start and end time...
         breakdown_start = self.browser.find_element_by_id('id_start_time')
         breakdown_start.send_keys('2009-10-25 14:30')
@@ -46,8 +50,19 @@ class BreakdownRegistrationTest(FunctionalTest):
         breakdown_end.send_keys('2009-10-26 14:30')
 
     # and enter short description of breakdown
-        description = self.browser.find_element_by_id('id_breakdown_description')
-        descriptions.send_keys('Main motor failure')
+        description = self.browser.find_element_by_id(
+            'id_breakdown_description')
+        description.send_keys('Main motor failure')
 
     # and press 'save' button
         self.browser.find_element_by_id('save_breakdown').click()
+
+    # message about succesful breakdown save is displayed
+        message = self.browser.find_element_by_id('test_message')
+#        all_messages = [message.get_attribute('text') for message in messages]
+        self.browser.implicitly_wait(10)
+        self.assertEqual('Breakdown successfully saved.', message.text)
+
+    # created failure is now displayed under the form
+        last_breakdowns = self.browser.find_element_by_id('last_breakdowns')
+        self.assertIn('Main motor failure', last_breakdowns)
